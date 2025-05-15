cf136b3a6a5e61464781b12984e78e7f76e9c228
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-05-15 13:19:44 +0600

    introduction to local storage , added local storage utility funciton

---
+++ src/Utilites/localstorage.js
@@ -0,0 +1,30 @@
+/***
+ * to get some thing from local storage , you will get it as a string
+ * convert this to javascript object/array
+ */
+
+const getCardFromLocalStorage=()=>{
+    const storedCartString = localStorage.getItem('cart');
+
+    if(storedCartString){
+        const storedCart= JSON.parse(storedCartString)
+        return storedCart;
+    }
+    return [];
+}
+  
+const saveCartToLocalStorage = cart=> {
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart',cartStringified)
+}
+
+ const addItemToCartLocalStorage = id =>{
+    const cart = getCardFromLocalStorage();
+    // cart.push(id);
+
+    const newCart = [...cart,id]
+    // save new cart to the local storage  
+    saveCartToLocalStorage(newCart)
+ }
+
+ export{getCardFromLocalStorage , addItemToCartLocalStorage}
